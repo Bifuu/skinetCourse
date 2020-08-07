1b3d591e5ba659731ce28ce31d7d27561562fa95
Dustin Stover <DustinStover@gmail.com>
2020-08-07 15:26:24 -0400

    Add Error handling and Swagger

---
+++ API/Errors/ApiResponse.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace API.Errors
+{
+  public class ApiResponse
+  {
+    public ApiResponse(int statusCode, string message = null)
+    {
+      StatusCode = statusCode;
+      Message = message ?? GetDefaultMessageForStatusCode(statusCode);
+    }
+
+    public int StatusCode { get; set; }
+    public string Message { get; set; }
+
+    private string GetDefaultMessageForStatusCode(int statusCode)
+    {
+      return statusCode switch
+      {
+        400 => "You have made a bad request.",
+        401 => "You are not Authorized.",
+        404 => "Resource was not found.",
+        500 => "Internal Server error.",
+        _ => null
+      };
+    }
+  }
+}
--- API/Startup.cs
+++ API/Startup.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Errors;
 using API.Helpers;
+using API.Middleware;
 using AutoMapper;
 using Core.Interfaces;
 using Infrastructure.Data;
@@ -15,6 +17,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace API
 {
@@ -35,15 +38,37 @@ namespace API
       services.AddAutoMapper(typeof(MappingProfiles));
       services.AddControllers();
       services.AddDbContext<StoreContext>(x => x.UseSqlite(_configuration.GetConnectionString("DefaultConnection")));
+
+      services.Configure<ApiBehaviorOptions>(options =>
+      {
+        options.InvalidModelStateResponseFactory = actionContext =>
+        {
+          var errors = actionContext.ModelState.Where(e => e.Value.Errors.Count > 0).SelectMany(x => x.Value.Errors).Select(x => x.ErrorMessage).ToArray();
+
+          var errorResponse = new ApiValidationErrorResponse
+          {
+            Errors = errors
+          };
+
+          return new BadRequestObjectResult(errorResponse);
+        };
+      });
+      services.AddSwaggerGen(c =>
+      {
+        c.SwaggerDoc("v1", new OpenApiInfo
+        {
+          Title = "SkiNet APi",
+          Version = "v1"
+        });
+      });
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
-      if (env.IsDevelopment())
-      {
-        app.UseDeveloperExceptionPage();
-      }
+      app.UseMiddleware<ExceptionMiddleware>();
+
+      app.UseStatusCodePagesWithReExecute("/errors/{0}");
 
       app.UseHttpsRedirection();
 
@@ -52,6 +77,9 @@ namespace API
       app.UseStaticFiles();
 
       app.UseAuthorization();
+
+      app.UseSwagger();
+      app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "SkiNet API v1"));
 
       app.UseEndpoints(endpoints =>
       {
