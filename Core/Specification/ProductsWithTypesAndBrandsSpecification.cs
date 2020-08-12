cf0ea9678e1660b561e3260687a798f9eab956c7
Dustin Stover <DustinStover@gmail.com>
2020-08-12 13:53:21 -0400

    ad Filtering by type and brand

--- Core/Specification/ProductsWithTypesAndBrandsSpecification.cs
+++ Core/Specification/ProductsWithTypesAndBrandsSpecification.cs
@@ -10,7 +10,10 @@ namespace Core.Specification
     /// </summary>
     public class ProductsWithTypesAndBrandsSpecification : BaseSpecification<Product>
     {
-        public ProductsWithTypesAndBrandsSpecification(string sort)
+        public ProductsWithTypesAndBrandsSpecification(string sort, int? brandId, int? typeId)
+            : base(x =>
+                (!brandId.HasValue || x.ProductBrandId == brandId) &&
+                (!typeId.HasValue || x.ProductTypeId == typeId))
         {
             AddInclude(x => x.ProductType);
             AddInclude(X => X.ProductBrand);
