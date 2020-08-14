c710dccf6d5fceb8c987dc807fb440ea5b8cdac3
Dustin Stover <DustinStover@gmail.com>
2020-08-14 13:22:22 -0400

    Add Error handling and some test errors

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -3,9 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { RouterModule } from '@angular/router';
+import { TestErrorComponent } from './test-error/test-error.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ServerErrorComponent } from './server-error/server-error.component';
 
 @NgModule({
-  declarations: [NavBarComponent],
+  declarations: [NavBarComponent, TestErrorComponent, NotFoundComponent, ServerErrorComponent],
   imports: [CommonModule, RouterModule],
   exports: [NavBarComponent],
 })
