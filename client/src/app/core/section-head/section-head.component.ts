136698344c17cc0a01c0640ad3c15daba9c7e160
Dustin Stover <DustinStover@gmail.com>
2020-08-15 11:55:31 -0400

    use breadcrumbs to get the title

--- client/src/app/core/section-head/section-head.component.ts
+++ client/src/app/core/section-head/section-head.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { BreadcrumbService } from 'xng-breadcrumb';
 
 @Component({
   selector: 'app-section-head',
   templateUrl: './section-head.component.html',
-  styleUrls: ['./section-head.component.scss']
+  styleUrls: ['./section-head.component.scss'],
 })
 export class SectionHeadComponent implements OnInit {
+  breadcrumb$: Observable<any[]>;
 
-  constructor() { }
+  constructor(private bcService: BreadcrumbService) {}
 
   ngOnInit(): void {
+    this.breadcrumb$ = this.bcService.breadcrumbs$;
   }
-
 }
