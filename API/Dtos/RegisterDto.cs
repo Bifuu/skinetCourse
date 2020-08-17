62f86823ebe1feadb26a9876d1a289cce68abb23
Dustin Stover <DustinStover@gmail.com>
2020-08-17 18:29:46 -0400

    Add api validation

--- API/Dtos/RegisterDto.cs
+++ API/Dtos/RegisterDto.cs
@@ -1,9 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace API.Dtos
 {
     public class RegisterDto
     {
-        public string DisplayName { get; set; }
-        public string Email { get; set; }
+        [Required] public string DisplayName { get; set; }
+        [Required] [EmailAddress] public string Email { get; set; }
+        [Required]
+        [RegularExpression(@"(?=^.{6,10}$)(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*\s).*$", ErrorMessage = "Password Must have 1 Uppercase, 1 lowercase, 1 number, 1 special, and atleast 6 characters")]
         public string Password { get; set; }
     }
 }
