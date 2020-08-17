8120e260573033a56069f086f1ca65d56f7f06d9
Dustin Stover <DustinStover@gmail.com>
2020-08-17 14:57:11 -0400

    Auto migrate and seed data to identity db

--- API/Program.cs
+++ API/Program.cs
@@ -2,8 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Core.Entities.Identity;
 using Infrastructure.Data;
+using Infrastructure.Identity;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -12,36 +15,41 @@ using Microsoft.Extensions.Logging;
 
 namespace API
 {
-  public class Program
-  {
-    public static async Task Main(string[] args)
+    public class Program
     {
-      var host = CreateHostBuilder(args).Build();
-      using (var scope = host.Services.CreateScope())
-      {
-        var services = scope.ServiceProvider;
-        var loggerFactory = services.GetRequiredService<ILoggerFactory>();
-        try
+        public static async Task Main(string[] args)
         {
-          var context = services.GetRequiredService<StoreContext>();
-          await context.Database.MigrateAsync();
-          await StoreContextSeed.SeedAsync(context, loggerFactory);
-        }
-        catch (Exception ex)
-        {
-          var logger = loggerFactory.CreateLogger<Program>();
-          logger.LogError(ex, "An Error occured during migration");
-        }
-      }
-
-      host.Run();
-    }
-
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
+            var host = CreateHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
             {
-              webBuilder.UseStartup<Startup>();
-            });
-  }
+                var services = scope.ServiceProvider;
+                var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+                try
+                {
+                    var context = services.GetRequiredService<StoreContext>();
+                    await context.Database.MigrateAsync();
+                    await StoreContextSeed.SeedAsync(context, loggerFactory);
+
+                    var userManager = services.GetRequiredService<UserManager<AppUser>>();
+                    var IdentityContext = services.GetRequiredService<AppIdentityDbContext>();
+                    await IdentityContext.Database.MigrateAsync();
+                    await AppIdentityDbContextSeed.SeedUsersAsync(userManager);
+                }
+                catch (Exception ex)
+                {
+                    var logger = loggerFactory.CreateLogger<Program>();
+                    logger.LogError(ex, "An Error occured during migration");
+                }
+            }
+
+            host.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
 }
