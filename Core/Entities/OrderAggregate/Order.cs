dccd1d45d2d162c2884e1fa2fa82edd668180e3b
Dustin Stover <DustinStover@gmail.com>
2020-08-18 15:09:09 -0400

    typo

--- Core/Entities/OrderAggregate/Order.cs
+++ Core/Entities/OrderAggregate/Order.cs
@@ -5,12 +5,12 @@ namespace Core.Entities.OrderAggregate
 {
     public class Order : BaseEntity
     {
-        public Order(IReadOnlyList<OrderItem> orderItem, string buyerEmail, Address shipToAddress, DeliveryMethod deliveryMethod, decimal subtotal)
+        public Order(IReadOnlyList<OrderItem> orderItems, string buyerEmail, Address shipToAddress, DeliveryMethod deliveryMethod, decimal subtotal)
         {
             BuyerEmail = buyerEmail;
             ShipToAddress = shipToAddress;
             DeliveryMethod = deliveryMethod;
-            OrderItem = orderItem;
+            OrderItems = orderItems;
             Subtotal = subtotal;
         }
 
@@ -18,7 +18,7 @@ namespace Core.Entities.OrderAggregate
         public DateTimeOffset OrderDate { get; set; } = DateTimeOffset.Now;
         public Address ShipToAddress { get; set; }
         public DeliveryMethod DeliveryMethod { get; set; }
-        public IReadOnlyList<OrderItem> OrderItem { get; set; }
+        public IReadOnlyList<OrderItem> OrderItems { get; set; }
         public decimal Subtotal { get; set; }
         public OrderStatus Status { get; set; } = OrderStatus.Pending;
         public string PaymentIntentId { get; set; }
