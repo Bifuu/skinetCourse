9c8f15beabeb7066a0a47dd86367aa76320a84ba
Dustin Stover <DustinStover@gmail.com>
2020-08-18 13:49:50 -0400

    Async validate email addy

--- client/src/app/account/account.service.ts
+++ client/src/app/account/account.service.ts
@@ -50,6 +50,7 @@ export class AccountService {
       map((user: IUser) => {
         if (user) {
           localStorage.setItem('token', user.token);
+          this.currentUserSource.next(user);
         }
       })
     );
@@ -62,6 +63,6 @@ export class AccountService {
   }
 
   checkEmailExists(email: string): Observable<object> {
-    return this.http.get(this.baseUrl + '/account/emailexists?email=' + email);
+    return this.http.get(this.baseUrl + 'account/emailexists?email=' + email);
   }
 }
