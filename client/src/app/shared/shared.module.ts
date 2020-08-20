d7739a11f4ffed581f7e4065be580d3533e3a772
Dustin Stover <DustinStover@gmail.com>
2020-08-19 20:58:23 -0400

    Lay groundwork for a stepper component

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -8,6 +8,8 @@ import { OrderTotalsComponent } from './components/order-totals/order-totals.com
 import { ReactiveFormsModule } from '@angular/forms';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { TextInputComponent } from './components/text-input/text-input.component';
+import { CdkStepperModule } from '@angular/cdk/stepper';
+import { StepperComponent } from './components/stepper/stepper.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +17,7 @@ import { TextInputComponent } from './components/text-input/text-input.component
     PagerComponent,
     OrderTotalsComponent,
     TextInputComponent,
+    StepperComponent,
   ],
   imports: [
     CommonModule,
@@ -22,6 +25,7 @@ import { TextInputComponent } from './components/text-input/text-input.component
     CarouselModule.forRoot(),
     BsDropdownModule.forRoot(),
     ReactiveFormsModule,
+    CdkStepperModule,
   ],
   exports: [
     PaginationModule,
@@ -32,6 +36,8 @@ import { TextInputComponent } from './components/text-input/text-input.component
     ReactiveFormsModule,
     BsDropdownModule,
     TextInputComponent,
+    CdkStepperModule,
+    StepperComponent,
   ],
 })
 export class SharedModule {}
