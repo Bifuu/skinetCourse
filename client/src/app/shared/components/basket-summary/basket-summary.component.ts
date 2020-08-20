feda0fb0a2d2230e284b022e6a0ad7cc532fdaaf
Dustin Stover <DustinStover@gmail.com>
2020-08-19 23:40:46 -0400

    restyle and use

--- client/src/app/shared/components/basket-summary/basket-summary.component.ts
+++ client/src/app/shared/components/basket-summary/basket-summary.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Observable } from 'rxjs';
 import { BasketService } from 'src/app/basket/basket.service';
 import { IBasket, IBasketItem } from '../../models/basket';
@@ -17,6 +17,7 @@ export class BasketSummaryComponent implements OnInit {
     IBasketItem
   >();
   @Output() remove: EventEmitter<IBasketItem> = new EventEmitter<IBasketItem>();
+  @Input() showControls = true;
 
   constructor(private basketService: BasketService) {}
 
